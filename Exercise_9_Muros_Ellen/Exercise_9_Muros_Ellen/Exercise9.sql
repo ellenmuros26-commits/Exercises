ac9931f91767d7b6fabf17070a59e5fc93f58683
ellenmuros26-commits <ellenmuros8@gmail.com>
2025-11-30 11:24:46 +0800

    Add files via upload

---
+++ Exercise_9_Muros_Ellen/Exercise_9_Muros_Ellen/Exercise9.sql
@@ -0,0 +1,23 @@
+use employees;
+/*1. List the minimum perks package.*/
+SELECT MIN(perks)
+FROM employee_data;
+
+
+/*2. List the maximum salary given to a "Programmer".*/
+SELECT MAX(salary)
+FROM employee_data
+WHERE title = 'Programmer';
+
+
+/*3. Display the age of the oldest "Marketing Executive".*/
+SELECT MAX(age)
+FROM employee_data
+WHERE title = 'Marketing Executive';
+
+
+/*4. (Tricky!) Find the first and last names of the oldest employee.*/
+SELECT f_name, l_name, age
+FROM employee_data
+ORDER BY age DESC
+LIMIT 1;
