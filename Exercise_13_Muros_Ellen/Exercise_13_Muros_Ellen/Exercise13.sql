896ef58db3c5f7aa6db5120d1c18d31959973f2b
ellenmuros26-commits <ellenmuros8@gmail.com>
2025-11-30 11:31:57 +0800

    Add files via upload

---
+++ Exercise_13_Muros_Ellen/Exercise_13_Muros_Ellen/Exercise13.sql
@@ -0,0 +1,10 @@
+Use employees;
+
+/*1. Which command displays the MySQL version?*/
+SELECT VERSION() AS mysql_version;
+
+/* 2. Use the SELECT command to evaluate 4 X 4 X 4 and name the column Cube of 4.*/
+SELECT (4 * 4 * 4) AS "Cube of 4";
+
+/*3. Display your name with SELECT.*/
+SELECT 'Ellen Muros' AS Name;
