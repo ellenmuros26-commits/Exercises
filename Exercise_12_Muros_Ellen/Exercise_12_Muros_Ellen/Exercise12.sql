f6be7cdaf81261777d2b7d9d2d922ff8bc8e055b
ellenmuros26-commits <ellenmuros8@gmail.com>
2025-11-30 11:31:20 +0800

    Add files via upload

---
+++ Exercise_12_Muros_Ellen/Exercise_12_Muros_Ellen/Exercise12.sql
@@ -0,0 +1,7 @@
+Use employees;
+
+/*1. List departments and average ages where the average age in more than 30.*/
+SELECT title, AVG(age) AS avg_age
+FROM employee_data
+GROUP BY title
+HAVING AVG(age) > 30;
