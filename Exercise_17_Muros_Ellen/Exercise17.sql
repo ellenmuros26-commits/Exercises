32b2d01a311b464ffd25ed1d5538192fba9fc250
ellenmuros26-commits <ellenmuros8@gmail.com>
2025-12-02 19:07:19 +0800

    Add files via upload

---
+++ Exercise_17_Muros_Ellen/Exercise17.sql
@@ -0,0 +1,33 @@
+use addressbook;
+/*1. ﻿﻿﻿Find and list the ids and spouse names of all employees who are married.*/
+SELECT e_id, s_name
+FROM employee_per
+WHERE m_status = 'Y';
+
+/*2. ﻿﻿﻿Change the above so that the display is sorted on spouse names.*/ 
+SELECT e_id, s_name
+FROM employee_per
+WHERE m_status = 'Y'
+ORDER BY s_name;
+
+/*3. ﻿﻿﻿How many employees do we have under each sex (male and female)?*/
+SELECT sex, COUNT(*) AS total
+FROM employee_per
+GROUP BY sex;
+
+/*4. ﻿﻿﻿How many of our employees are married and unmarried?*/
+SELECT m_status, COUNT(*) AS total
+FROM employee_per
+GROUP BY m_status;
+
+/*5. ﻿﻿﻿Find the total number of children.*/
+SELECT SUM(children) AS total_children
+FROM employee_per;
+
+/*6. ﻿﻿﻿Make unique groups of children and find the number in each group. Sort the display with the group having maximum children, at the top.*/
+SELECT children AS child_count,
+       COUNT(*) AS total_employees
+FROM employee_per
+WHERE children IS NOT NULL
+GROUP BY children
+ORDER BY total_employees DESC;
